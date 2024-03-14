0e975ccc5388ea7c6b1910fedc10ca3cac6a2985
Luis Soares Brasil <llhenriquesoares@gmail.com>
2024-03-14 18:27:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ const tasks = [
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
 
+function renderElements(array){
+  const elementUl = document.createElement("ul");
+
+}
+
+function createTaskItem(title,type){
+  const contador = title.length;
+  while(contador>=0){
+    const elementTaskLi[contador] = document.createElement("li");
+    const elementTaskDiv[contador] = document.createElement("div");
+    const elementTaskSpan[contador] = document.createElement("span");
+    const elementTaskP[contador] = document.createElement("p");
+    elementTaskP[contador].innerText = title; 
+    const elementTaskButton[contador] = document.createElement("button");
+    
+    elementTaskLi[contador].appendChild(elementTaskDiv);
+    elementTaskDiv[contador].appendChild(elementTaskSpan);
+    elementTaskDiv[contador].appendChild(elementTaskP);
+    elementTaskLi[contador].appendChild(elementTaskButton);
+  
+    elementTaskLi[contador].classList.add("task__item");
+    elementTaskDiv.classList.add("task-info__container");
+    elementTaskSpan[contador].classList.add("task-type");
+    if(type[contador]=="Normal"){
+      elementTaskSpan[contador].classList.add("span-normal")
+    }else if(type[contador]=="Importante"){
+      elementTaskSpan[contador].classList.add("span-important");
+    }else{
+      elementTaskSpan[contador].classList.add("span-urgent")
+    }
+    elementTaskButton.classList.add("task__button--remove-task");
+
+  }
+}
